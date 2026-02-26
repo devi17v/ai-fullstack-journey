dc564e614e50019825d855ad479a6743c8a9de77
devi17v <devi17velusamy@gmail.com>
2026-02-26 11:02:14 +0530

    CRUD operattion with validation

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 5000
-const posts = require('./routes/post')
+const users = require('./routes/user')
 
 const app = express()
 
@@ -10,6 +10,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 //Routes
-app.use('/api/posts', posts)
+app.use('/api/users', users)
 
 app.listen(port, () => console.log(`server is running successfully ${port}`))
